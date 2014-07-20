13b71aa3a7208ff7cf7cdf00e2043ee38caaa00a
Quentin Conner <quentin@mongodb.com>
2014-07-20 11:51:53 -0500

    fix misspelling and resolve conflict from moved files

--- src/mongo/db/catalog/collection.cpp
+++ src/mongo/db/catalog/collection.cpp
@@ -404,14 +404,14 @@ namespace mongo {
 
     Status Collection::updateDocumentWithDamages( OperationContext* txn,
                                                   const DiskLoc& loc,
-                                                  const char* damangeSource,
+                                                  const char* damageSource,
                                                   const mutablebson::DamageVector& damages ) {
 
         // Broadcast the mutation so that query results stay correct.
         _cursorCache.invalidateDocument(loc, INVALIDATION_MUTATION);
 
         ExclusiveResourceLock lk(txn->getTransaction(), *(size_t*)&loc);
-        return _recordStore->updateWithDamages( txn, loc, damangeSource, damages );
+        return _recordStore->updateWithDamages( txn, loc, damageSource, damages );
     }
 
     bool Collection::_enforceQuota( bool userEnforeQuota ) const {
