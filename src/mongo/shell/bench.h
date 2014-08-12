a53fd1a88ed8470f1eeabd59a8868af7346f4ac6
Quentin Conner <quentin@mongodb.com>
2014-08-11 21:00:09 -0500

    CAP-247 variance+write concern patch applied to 2.7.4

--- src/mongo/shell/bench.h
+++ src/mongo/shell/bench.h
@@ -413,6 +413,8 @@ namespace mongo {
 
         const BenchRunConfig &config() const { return *_config; } // TODO: Remove this function.
 
+        static double round(double d);
+
         // JS bindings
         static BSONObj benchFinish(const BSONObj& argsFake, void* data);
         static BSONObj benchStart(const BSONObj& argsFake, void* data);
@@ -425,6 +427,8 @@ namespace mongo {
 
         OID _oid;
         BenchRunState _brState;
+        Timer *_brTimer;
+        unsigned long long _microsElapsed;
         boost::scoped_ptr<BenchRunConfig> _config;
         std::vector<BenchRunWorker *> _workers;
 
