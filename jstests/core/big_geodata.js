4a812b13251d71875d976b75c3b0cdafa7cf2bdd
Quentin Conner <quentin@mongodb.com>
2014-09-23 14:55:13 -0500

    more polygon data

--- jstests/core/big_geodata.js
+++ jstests/core/big_geodata.js
@@ -123,5 +123,128 @@ coll.insert(polyWithTwoHoles);
 
 
 // sanity check
-assert.eq(coll.count(), 12, 'test data (Polygons) insert failed');
+assert.eq(coll.count(), 12, 'test data (Polygons 1) insert failed');
+
+
+
+
+// Polygon covering north pole
+var polyNP = {};
+polyNP.geo = {};
+polyNP.name = 'polygon covering North pole';
+polyNP.geo.type = 'Polygon';
+polyNP.geo.coordinates = [
+              [ [ -120.0, 89.0 ], [ 0.0, 89.0 ], [ 120.0, 89.0 ], [ -120.0, 89.0 ] ]
+            ];
+coll.insert(polyNP);
+
+// Polygon covering south pole
+var polySP = {};
+polySP.geo = {};
+polySP.name = 'polygon covering South pole';
+polySP.geo.type = 'Polygon';
+polySP.geo.coordinates = [
+              [ [ -120.0, -89.0 ], [ 0.0, -89.0 ], [ 120.0, -89.0 ], [ -120.0, -89.0 ] ]
+            ];
+coll.insert(polySP);
+
+
+// proper CRS object
+var CRS = {};
+CRS.type = 'name';
+CRS.properties = {};
+CRS.properties.name = 'urn:mongodb:strictwindingcrs:EPSG:4326';
+
+
+
+// Big Polygon covering both poles
+// this document will NOT be found in the 2.8 release
+// b/c this feature does not allow storage of big polygon objects
+var bigPoly = {};
+bigPoly.geo = {};
+bigPoly.name = 'big polygon/rectangle covering both poles';
+bigPoly.geo.type = 'Polygon';
+bigPoly.geo.crs = CRS;
+bigPoly.geo.coordinates = [
+              [ [ -130.0, 89.0 ], [ -120.0, 89.0 ], [ -120.0, -89.0 ], [ -130.0, -89.0 ], [ -130.0, 89.0 ] ]
+            ];
+coll.insert(bigPoly);
+
+// sanity check
+assert.eq(coll.count(), 14, 'test data (Polygons 2) insert failed');
+
+
+
+// Polygon w/ hole @ North pole
+var polyWithHole = {};
+polyWithHole.geo = {};
+polyWithHole.name = 'polygon (triangle) w/ hole at North pole';
+polyWithHole.geo.type = 'Polygon';
+polyWithHole.geo.coordinates = [
+              [ [ -120.0, 80.0 ], [ 0.0, 80.0 ], [ 120.0, 80.0 ], [-120.0, 80.0 ] ],
+              [ [ -120.0, 88.0 ], [ 0.0, 88.0 ], [ 120.0, 88.0 ], [-120.0, 88.0 ] ]
+            ];
+coll.insert(polyWithHole);
+
+
+
+
+// Polygon with edge on equator
+poly = {};
+poly.geo = {};
+poly.name = 'polygon with edge on equator';
+poly.geo.type = 'Polygon';
+poly.geo.coordinates = [
+              [ [ -120.0, 0.0 ], [ 120.0, 0.0 ], [ 0.0, 90.0 ], [ -120.0, 0.0 ] ]
+            ];
+coll.insert(poly);
+
+
+
+// Polygon just inside a single hemisphere (Northern)
+// China, California, Europe
+poly = {};
+poly.geo = {};
+poly.name = 'polygon just inside Northern hemisphere';
+poly.geo.type = 'Polygon';
+poly.geo.coordinates = [
+              [ [ 120.0, 0.000001 ], [ -120.0, 0.000001 ], [ 0.0, 0.000001 ], [ 120.0, 0.000001 ] ]
+            ];
+coll.insert(poly);
+
+poly = {};
+poly.geo = {};
+poly.name = 'polygon inside Northern hemisphere';
+poly.geo.type = 'Polygon';
+poly.geo.coordinates = [
+              [ [ 120.0, 80.0 ], [ -120.0, 80.0 ], [ 0.0, 80.0 ], [ 120.0, 80.0 ] ]
+            ];
+coll.insert(poly);
+
+
+
+// Polygon just inside a single hemisphere (Southern)
+// Pacific Ocean South of California, Indonesia, Africa
+poly = {};
+poly.geo = {};
+poly.name = 'polygon just inside Southern hemisphere';
+poly.geo.type = 'Polygon';
+poly.geo.coordinates = [
+              [ [ -120.0, -0.000001 ], [ 120.0, -0.000001 ], [ 0.0, -0.000001 ], [ -120.0, -0.000001 ] ]
+            ];
+coll.insert(poly);
+
+poly = {};
+poly.geo = {};
+poly.name = 'polygon inside Southern hemisphere';
+poly.geo.type = 'Polygon';
+poly.geo.coordinates = [
+              [ [ -120.0, -80.0 ], [ 120.0, -80.0 ], [ 0.0, -80.0 ], [ -120.0, -80.0 ] ]
+            ];
+coll.insert(poly);
+
+
+// sanity check
+assert.eq(coll.count(), 20, 'test data (Polygons 3) insert failed');
+
 
--- jstests/core/big_geoquery.js
+++ jstests/core/big_geoquery.js
@@ -23,24 +23,29 @@ assert.eq(0, curs.count(), 'expected no docs within shenzhen triangle');
 var CRS = {};
 CRS.type = 'name';
 CRS.properties = {};
-// no good but referenced at
+
+
+// this CRS string no good but referenced at
 // https://wiki.mongodb.com/display/10GEN/Multi-hemisphere+%28BigPolygon%29+queries
 CRS.properties.name = 'urn:mongodb:crs:strictwinding:EPSG:4326';
 
 shenzhenPoly.crs = CRS;
 curs = coll.find({geo: {$geoWithin: {$geometry: shenzhenPoly}}});
+assert.throws(function(c){
+    c.count();
+}, curs, 'expected error with bad CRS');
 
-// this form works for a Big Polygon geo query
+
+// this CRS form works for a Big Polygon geo query
 CRS.properties.name = 'urn:mongodb:strictwindingcrs:EPSG:4326';
 shenzhenPoly.crs = CRS;
 curs = coll.find({geo: {$geoWithin: {$geometry: shenzhenPoly}}});
-
 assert.eq(0, curs.count(), 'expected no docs within shenzhen triangle');
 
 
 
-
 // now query for objects outside the triangle.  reverse the coordinate traversal direction.
+// left foot walking traversal where left foot is inside the polygon
 shenzhenPoly.coordinates = [
                              [
                                [ 114.0834046, 22.6648202 ],
@@ -51,8 +56,19 @@ shenzhenPoly.coordinates = [
                            ];
 
 curs = coll.find({geo: {$geoWithin: {$geometry: shenzhenPoly}}});
-assert.eq(12, curs.count(), 'expected 12 docs outside shenzhen triangle');
+// all geos should be found except the polygon just inside the northern hemisphere
+// that (rather large) polygon covers the shenzhen triangle
+assert.eq(19, curs.count(), 'expected 19 docs outside shenzhen triangle');
+
+
+
+
+
+
+
+
 
 // debug
-//db.big_geodata.find({geo: {$geoWithin: {$geometry: shenzhenPoly}}}, {name: 1, 'geo.type': 1}).sort({'geo.type': 1, name: 1})
-
+db.big_geodata.count()
+db.big_geodata.find({}, {name: 1, 'geo.type': 1}).sort({'geo.type': 1, name: 1})
+db.big_geodata.find({geo: {$geoWithin: {$geometry: shenzhenPoly}}}, {name: 1, 'geo.type': 1}).sort({'geo.type': 1, name: 1})
